8829bb460d9a58dc25477a29e4bb52b45f33f64b
zastrixarundell <zastrixarundell@gmail.com>
2020-04-16 00:01:09 +0200

    Fixed typo in prod config

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :catlixir_web, CatlixirWebWeb.Endpoint,
   url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
