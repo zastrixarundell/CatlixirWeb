381c6eda20535585b304c67551cf58d2721d012b
zastrixarundell <zastrixarundell@gmail.com>
2020-04-16 20:17:11 +0200

    Added docs

---
+++ assets/static/api/docs/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Catlixir",
+  "version": "1.0.0",
+  "description": "Documentation for the Catlixir API Endpoints",
+  "title": "Catlixir Documentation",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-04-16T18:17:00.188Z",
+    "url": "http://apidocjs.com",
+    "version": "0.20.1"
+  }
+});
